a9b2718f1b09e39cdd884665dc750017557f5d48
Mr Naeem <160456738+ITNeemi@users.noreply.github.com>
2025-01-07 01:29:27 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+"use strict";
+let ref_section = document.getElementById("ref");
+let ref_btn = document.getElementById("ref_btn");
+ref_btn?.addEventListener("click", () => {
+    ref_section.style.display = "none";
+});
